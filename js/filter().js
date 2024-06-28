5eecf9d2b1052248190c38ebf1058b9bbd180c94
Dozorcevae <lena.dozorceva@gmail.com>
2024-06-28 20:47:42 +0700

    Create filter().js
    In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
    
    Example
    
    filter_list([1,2,'a','b']) == [1,2]
    filter_list([1,'a','b',0,15]) == [1,0,15]
    filter_list([1,2,'aasf','1','123',123]) == [1,2,123]

---
+++ js/filter().js
@@ -0,0 +1,20 @@
+//filter_list([1,2,'a','b']) == [1,2]
+
+function filter_list(l) {
+  // Используем метод filter для фильтрации элементов
+  return l.filter(item => typeof item === 'number');
+}
+//другие примеры искользования: 
+
+
+//Фильтрация чётных чисел:
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // Вывод: [2, 4, 6, 8, 10]
+
+
+//Фильтрация элементов, присутствующих в другом массиве:
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [2, 4];
+const filteredArr = arr1.filter(num => arr2.includes(num));
+console.log(filteredArr); // Вывод: [2, 4]
