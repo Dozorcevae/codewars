05ce1b1163332048ff179182732bad014a59018d
Dozorcevae <lena.dozorceva@gmail.com>
2024-06-28 22:07:57 +0700

    фильтрация массивов по определенному параметру

--- js/filter().js
+++ js/filter().js
@@ -7,7 +7,7 @@ function filter_list(l) {
 //другие примеры искользования: 
 
 
-//Фильтрация чётных чисел:
+//Фильтрация чётных чисел: 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = numbers.filter(num => num % 2 === 0);
 console.log(evenNumbers); // Вывод: [2, 4, 6, 8, 10]
