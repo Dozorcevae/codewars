346643693fe012c7902442dba8cb9bb7c51b4e69
Dozorcevae <lena.dozorceva@gmail.com>
2024-06-28 22:09:08 +0700

    массив -> число (2) -> число(10)

--- js/bin_to_10.js
+++ js/bin_to_10.js
@@ -1,5 +1,5 @@
 //Testing: [0, 0, 1, 0] ==> 2
 
 const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
-
+ 
 //parseInt приводит к int, arr.join('') -- массив, объединенный в строку в сис-ме исчесления 2
