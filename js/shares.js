d14b32d08cc6997473bdf1d4c6355fbf49271abd
Dozorcevae <lena.dozorceva@gmail.com>
2024-06-28 21:27:11 +0700

    возведение в степень каждой отдельной цифры

--- js/shares.js
+++ js/shares.js
@@ -1,4 +1,4 @@
-//9119 through the function, 811181 (81-1-1-81)
+//9119 through the function, 811181 (81-1-1-81) (кубы каждой цифры числа)
 
 function squareDigits(num){
   return +num.toString().split('').map(n => n*n).join('');
