fc76d883738a0084172693105f368ca6ff803861
Dozorcevae <lena.dozorceva@gmail.com>
2024-06-29 13:23:44 +0700

    фильтрация массива

--- js/filter().js
+++ js/filter().js
@@ -18,3 +18,8 @@ const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [2, 4];
 const filteredArr = arr1.filter(num => arr2.includes(num));
 console.log(filteredArr); // Вывод: [2, 4]
+
+//имя = слово из 4-х символов:
+function friend(friends){
+  return friends.filter(name => name.length == 4);
+}
